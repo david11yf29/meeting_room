2d25a70fc2a542a98ee6bc141bccb86f86c84c7c
david11yf29 <david11yf29@gmail.com>
2026-03-27 00:28:25 +0800

    change auto-formatting setting and add cpp templates

--- src/OASystemUI.cpp
+++ src/OASystemUI.cpp
@@ -1,3 +1,14 @@
 #include "OASystemUI.h"
+#include <iostream>
+using namespace std;
 
-void OASystemUI::displayMainPage() {}
+void OASystemUI::displayMainPage() {
+    while (1) {
+        cout << "Welcome to the OA Meeting Reservation System" << endl;
+        cout << "----------------------------------------" << endl;
+        cout << "1. Admin Login" << endl;
+        cout << "2. User Login" << endl;
+        cout << "3. Quit" << endl;
+        cout << "Please Select a number" << endl;
+    }
+}
