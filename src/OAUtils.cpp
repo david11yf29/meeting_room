7f1d1c467b2d5f14569ae876db60244753f70201
David Lin <davidlin@TWTDMB25091.local>
2026-03-27 13:53:15 +0800

    20260327-1

--- src/OAUtils.cpp
+++ src/OAUtils.cpp
@@ -1,2 +1,15 @@
-#pragma once
-class OAUtils {};
+#include "OAUtils.h"
+#include <iostream>
+using namespace std;
+
+int OAUtils::inputNumber() {
+    int choice = -1;
+    cin >> choice;
+    while (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> choice;
+    }
+
+    return choice;
+};
